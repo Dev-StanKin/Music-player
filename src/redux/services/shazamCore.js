01bdc4799f1ca705aa4e6f985b68bfc17f02f4ca
Stanley Waweru <stanleywaweru79@gmail.com>
2023-09-08 07:09:28 +0300

    Updated API endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,36 +3,37 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/',
+    baseUrl: 'https://shazam.p.rapidapi.com',
     prepareHeaders: (headers) => {
       headers.set(
         'X-RapidAPI-Key',
-        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+        'b6487e1c13msh7b585e03f4460ffp182bbcjsn092089acda50'
       );
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getTopCharts: builder.query({ query: () => 'charts/track' }),
     getSongsByGenre: builder.query({
-      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+      query: (genre) => `charts/list/global?name=${genre}`,
     }),
     getSongsByCountry: builder.query({
-      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+      query: (countryCode) => `charts/list/country?country_code=${countryCode}`,
     }),
     getSongsBySearch: builder.query({
       query: (searchTerm) =>
-        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
     }),
     getArtistDetails: builder.query({
-      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+      query: (artistId) => `/artists/get-details?id=${artistId}`,
     }),
     getSongDetails: builder.query({
-      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+      query: ({ songid }) => `songs/v2/get-details?id=${songid}`,
     }),
     getSongRelated: builder.query({
-      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+      query: ({ songid }) =>
+        `shazam-songs/list-similarities?track_id=${songid}`,
     }),
   }),
 });
